c6892b0beaed52d15ce8f79803db4f7ee9622a42
Vera Timova <vera.timova@gmail.com>
2023-03-17 22:36:52 +0100

    Sign up with email and password to be completed

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+}
+
+const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formFields;
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if(password !== confirmPassword){
+            alert("password do not match");
+            return;
+        }
+
+        try {
+            const response = await createAuthUserWithEmailAndPassword(email, password);
+        }catch(error){
+
+        }
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormFields({...formFields, [name]:value });
+    };
+
+    return(
+        <div>
+            <h1>Sign up with your email and password</h1>
+            <form onSubmit={() => {}}>
+                <label>Display name</label>
+                <input type='text' required onChange={handleChange} name="displayName" value={displayName}/>
+                
+                <label>Email</label>
+                <input type='email' required onChange={handleChange} name="email" value={email}/>
+                
+                <label>Password</label>
+                <input type='password' required onChange={handleChange} name="password" value={password}/>
+                
+                <label>Confirm Password</label>
+                <input type='password' required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
+                <button type='submit'>Sign up</button>
+            </form>
+        </div>
+    )
+};
+
+export default SignUpForm;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,70 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCPxluhqxqwassWVfLz3vi1XbFoqMgPF6I",
+  
+    authDomain: "crwn-clothing-db-9166d.firebaseapp.com",
+  
+    projectId: "crwn-clothing-db-9166d",
+  
+    storageBucket: "crwn-clothing-db-9166d.appspot.com",
+  
+    messagingSenderId: "751145588850",
+  
+    appId: "1:751145588850:web:f40953f2116321dbbc4aeb"
+  
+  };
+  
+  
+  // Initialize Firebase
+  
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const googleProvider = new GoogleAuthProvider();
+  
+  googleProvider.setCustomParameters({
+    prompt: "select_account"
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    if(!userAuth) return;
+
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+    
+    //if user data does not exist
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName, 
+                email, 
+                createdAt
+            });
+        }catch(error){
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    //if user data exist
+    return userDocRef;
+  };
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
+  };
