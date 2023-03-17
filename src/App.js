8ea27f831a07fced93a92f3b61f6e55578fbcd03
Vera Timova <vera.timova@gmail.com>
2023-03-17 13:14:20 +0100

    Categories completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Directory from './components/directory/directory.component';
 
 const App = () => {
 
@@ -6,37 +6,32 @@ const App = () => {
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: 	"https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
       id: 4,
       title: 'Women',
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
       id: 5,
       title: 'Men',
+      imageUrl: 	"https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
 
   return (
-    <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category-container">
-          <div className='background-image' />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 }
 
